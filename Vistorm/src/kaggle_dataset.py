3cfa7a448ac6721cb8c708979bc46118921b61b4
giulsp ⅋ <62295517+queenofh3ll20@users.noreply.github.com>
2025-01-29 19:13:40 +0100

    Update kaggle_dataset.py

--- Vistorm/src/kaggle_dataset.py
+++ Vistorm/src/kaggle_dataset.py
@@ -3,16 +3,17 @@ import os
 import pandas as pd
 import json
 import shutil
+from datetime import datetime, timedelta
 
-# Define the destination for the dataset file
+# Definisce la destinazione per il file dataset
 destination_path = "/kaggle"
 os.makedirs(destination_path, exist_ok=True)
 
-# Download the dataset
+# Scarica il dataset
 path = kagglehub.dataset_download("asaniczka/top-spotify-songs-in-73-countries-daily-updated")
 print("Path to dataset files:", path)
 
-# Find the specific dataset file
+# Trova il file dataset
 dataset_file = None
 for root, _, files in os.walk(path):
     for file in files:
@@ -26,12 +27,12 @@ if not dataset_file:
     print("No dataset.csv file found!")
     exit()
 
-# Move the dataset file to the destination
+# Sposta il file alla destinazione
 destination_file = os.path.join(destination_path, os.path.basename(dataset_file))
 shutil.move(dataset_file, destination_file)
 print(f"Dataset file moved to: {destination_file}")
 
-# List of countries
+# Lista dei Countries
 countries_to_include = [
     # Nord America
     "CA", "US", "MX",
@@ -52,20 +53,28 @@ countries_to_include = [
     "AU", "NZ"
 ]
 
-# Convert the CSV to JSON, keeping only the specified countries
+# Data di oggi
+today_date = datetime.today().strftime('%Y-%m-%d')
+yesterday_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+
+# Converte il CSV a JSON, mantenendo solo i paesi selezionati
 json_file = os.path.splitext(destination_file)[0] + ".ndjson"
 
 try:
-    # Read the CSV
+    # Legge il CSV
     df = pd.read_csv(destination_file)
     
-    # Filter the rows for the specified countries
+    # Filtra le righe per paesi
     df_filtered = df[df['country'].isin(countries_to_include)]
+
+    # Filtra per data (mantieni solo la data odierna)
+    df_filtered_recent = df_filtered[df_filtered['snapshot_date'].isin([today_date, yesterday_date])]
     
-    # Remove duplicates based on the 'Spotify_id' column
-    df_filtered_no_duplicates = df_filtered.drop_duplicates(subset='spotify_id', keep='first')
+    # Rimuove i duplicati basati sulla colonn 'spotify_id'
+    df_filtered_no_duplicates = df_filtered_recent.drop_duplicates(subset='spotify_id', keep='first')
     
-    # Convert the filtered DataFrame to JSON
+    # Converte il DataFrame filtrato a JSON
     df_filtered_no_duplicates.to_json(json_file, orient="records", lines=True)
     print(f"Dataset converted to JSON: {json_file}")
 except Exception as e:
